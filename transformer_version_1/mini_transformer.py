13cda118a1c5657180c4a22f4517f3ea86d25418
Tensor <narutokelechi@gmail.com>
2024-04-26 21:28:11 +0100

    Added Multihead attention and connection layer

--- transformer_version_1/mini_transformer.py
+++ transformer_version_1/mini_transformer.py
@@ -12,7 +12,7 @@ class InputEmbedding(nn.Module):
     def forward(self, x):
         embedding = self.embedding(x) * math.sqrt(self.d_model)
         return embedding
-    
+
 
 class PositionalEncoding(nn.Module):
     def __init__(self, seq_len, d_model, dropout):
@@ -35,8 +35,8 @@ class PositionalEncoding(nn.Module):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
         
         return self.dropout(x)
-    
-    
+
+
 class LayerNormalization(nn.Module):
     def __init__(self, epsilon: float = 10**-6):
         super().__init__()
@@ -49,7 +49,7 @@ class LayerNormalization(nn.Module):
         std = x.std(dim = -1, keepdim = True)
         
         return self.alpha * (x - mean) / (std * self.eps) + self.bias
-     
+
 
 class FeedForwardLayer(nn.Module):
     def __init__(self, d_model, d_ff, dropout):
@@ -62,5 +62,62 @@ class FeedForwardLayer(nn.Module):
         x = self.linear1(x)
         x = self.dropout(torch.relu(x))
         x = self.linear2(x)
-        
 
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model, nh, dropout) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.nh = nh
+        assert d_model % nh == 0, 'd_model is not divisble by nh'
+        self.d_k = d_model // nh
+
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_o = nn.Linear(d_model, d_model)
+
+        self.dropout = nn.Dropout(dropout)
+    
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+            
+        attention_scores = attention_scores.softmax(dim = -1)
+        
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        return (attention_scores @ value), attention_scores
+            
+        
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        query = query.view(query.shape[0], query.shape[1], self.nh, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.nh, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.nh, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+        x =  x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.nh * self.d_k)
+        
+        return self.w_o(x)
+    
+
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout):
+        super().__init__()
+        self.dropout = dropout
+        self.norm = LayerNormalization()
+        
+    def forward(self, x, sublayer):
+        x = sublayer(self.norm(x))
+        x = self.dropout(x)
+        
+        return x + self.dropout(sublayer(self.norm(x)))
+    
