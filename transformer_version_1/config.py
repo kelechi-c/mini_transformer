83f64f038c83336858aa4af59831717d1ea712a7
Tensor <narutokelechi@gmail.com>
2024-04-27 15:40:59 +0100

    Model parameters

---
+++ transformer_version_1/config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "lr": 10**-4,
+        "epochs": 25,
+        "seq_len": 350,
+        "d_model": 512,
+        "lang_src": "en",
+        "lang_tgt": "it",
+        "model_folder": "weights",
+        "model_file": "mini_transformer_",
+        "preload": None,
+        "tokenizer_file": "tokenizer_{0}.json",
+        "exp_name": "runs/mini_transformer",
+    }
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = config['model_folder']
+    model_file = config['model_file']
+    model_filename = f'{model_file}{epoch}.pt'
+    return str(Path('.')/model_folder/model_filename)
+
